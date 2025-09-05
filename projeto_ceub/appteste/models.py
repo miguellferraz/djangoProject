68bdc8379c584ab89b9ce77d499239e100217a5b
miguellferraz <miguelclferraz@gmail.com>
2025-09-05 10:44:18 -0300

    Add initial migration for Produto model and update templates

--- projeto_ceub/appteste/models.py
+++ projeto_ceub/appteste/models.py
@@ -3,3 +3,12 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return f'Produto: {self.nome}-{self.preco}'
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
