67232b99fc421f48365d2d5db30df8b9b6004e7a
miguellferraz <miguelclferraz@gmail.com>
2025-09-12 09:33:43 -0300

    Add base template and additional pages for the application

--- projeto_ceub/appteste/urls.py
+++ projeto_ceub/appteste/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Exemplo de view
-    path('sobre', views.sobre, name='sobre'),  # Exemplo de view
+    path('sobre', views.sobre, name='sobre'),
+    path('produtos/', views.lista_produtos, name='listar_produtos'),
+    path('contatos/', views.contatos, name='contatos'),
+    
 ]
+
--- projeto_ceub/appteste/views.py
+++ projeto_ceub/appteste/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
     return render(request, 'meu_app/index.html', {})
 
 def sobre(request):
     return render(request, 'meu_app/sobre.html', {})
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'meu_app/lista.html', {'produtos': produtos})
+    
+def contatos(request):
+    return render(request, 'meu_app/contato.html', {})
+
